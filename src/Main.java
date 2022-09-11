0ab46a12dcf06b87fe2b7eb76ea0d2bbd79bd007
renitanathania <renitans16@gmail.com>
2022-09-11 11:30:51 +0700

    tp1 no.3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String namaUser, namaBarang, separator;
+        int jumlahBarang;
+        double hargaBeli, hargaJual;
+
+        separator = "#########################";
+
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("Masukan nama anda : ");
+        namaUser = userInput.nextLine();
+        System.out.println(" ");
+
+        System.out.println("Stock Gudang Input Barang");
+        System.out.println(separator);
+        System.out.println(" ");
+
+        System.out.println("Selamat datang " + namaUser);
+        System.out.println(" ");
+
+        System.out.print("Masukan nama barang yang akan ditambah : ");
+        namaBarang = userInput.nextLine();
+        System.out.println(" ");
+
+        System.out.print("Masukan jumlah barang yang akan ditambah : ");
+        jumlahBarang = userInput.nextInt();
+        System.out.println(" ");
+
+        System.out.print("Masukan harga beli untuk barang tersebut : ");
+        hargaBeli = Math.round(userInput.nextDouble() * 100.0) / 100.0;
+        System.out.println(" ");
+
+        System.out.print("Masukan harga jual untuk barang tersebut : ");
+        hargaJual = Math.round(userInput.nextDouble() * 100.0) / 100.0;
+        System.out.println(" ");
+
+        System.out.println("Stock Gudang Rincian Barang");
+        System.out.println(separator);
+        System.out.println(" ");
+
+        System.out.println("Nama Barang : " + namaBarang);
+        System.out.println("Jumlah Barang : " + jumlahBarang);
+        System.out.println("Harga Beli : Rp. " + hargaBeli);
+        System.out.println("Harga Jual : Rp. " + hargaJual);
     }
 }
